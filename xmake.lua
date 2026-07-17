c544df32cbfb41d6b41b487e3015e6c866300872
Monitor221hz <monitor.144.hz.100.srgb@gmail.com>
2026-07-17 23:13:34 +0100

    license: change license

--- xmake.lua
+++ xmake.lua
@@ -3,7 +3,7 @@ local name = "WaitYourTurn"
 local author = "monitor221hz"
 local description = "SKSE64 plugin template using CommonLibSSE-NG"
 local version = "1.0.0"
-local license = "GPL-3.0"
+local license = "AGPL-3.0"
  
 -- set minimum xmake version
 set_xmakever("2.8.2")
