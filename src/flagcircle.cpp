30b030312865bd4e521e70413eb2e23c122eaff8
Monitor221hz <monitor.144.hz.100.srgb@gmail.com>
2026-07-24 02:37:10 +0100

    fix: merge error

--- src/flagcircle.cpp
+++ src/flagcircle.cpp
@@ -13,18 +13,17 @@ namespace WaitYourTurn
         auto *actor = RE::TESForm::LookupByID<RE::Actor>(id);
         if (!actor)
         {
-            auto* actor = RE::TESForm::LookupByID<RE::Actor>(id);
-            if (!actor) { return; }
-            actor->GetActorRuntimeData().boolFlags.set(RE::Actor::BOOL_FLAGS::kAttackingDisabled); 
-            actor->GetActorRuntimeData().boolFlags.set(RE::Actor::BOOL_FLAGS::kCastingDisabled); 
-            if (Settings::GetCircling().bDebugDisplay)
+            return;
+        }
+        actor->GetActorRuntimeData().boolFlags.set(RE::Actor::BOOL_FLAGS::kAttackingDisabled);
+        actor->GetActorRuntimeData().boolFlags.set(RE::Actor::BOOL_FLAGS::kCastingDisabled);
+        if (Settings::GetCircling().bDebugDisplay)
+        {
+            if (auto *mesh = actor->GetHeadPartObject(RE::BGSHeadPart::HeadPartType::kFace))
             {
-                if (auto *mesh = actor->GetHeadPartObject(RE::BGSHeadPart::HeadPartType::kFace))
-                {
-                    mesh->TintScenegraph(RE::NiColorA(0.f, 0.f, 0.f, 0.f));
-                }
+                mesh->TintScenegraph(RE::NiColorA(0.f, 0.f, 0.f, 0.f));
             }
-        });
+        }
     }
     void FlagCircle::StopCircling(RE::FormID combatMemberID)
     {
@@ -41,14 +40,12 @@ namespace WaitYourTurn
         }
         actor->GetActorRuntimeData().boolFlags.reset(RE::Actor::BOOL_FLAGS::kAttackingDisabled);
         actor->GetActorRuntimeData().boolFlags.reset(RE::Actor::BOOL_FLAGS::kCastingDisabled);
-
-            if (Settings::GetCircling().bDebugDisplay)
+        if (Settings::GetCircling().bDebugDisplay)
+        {
+            if (auto *mesh = actor->GetHeadPartObject(RE::BGSHeadPart::HeadPartType::kFace))
             {
-                if (auto *mesh = actor->GetHeadPartObject(RE::BGSHeadPart::HeadPartType::kFace))
-                {
-                    mesh->TintScenegraph(RE::NiColorA(0.f, 1.f, 0.f, 20.f));
-                }
+                mesh->TintScenegraph(RE::NiColorA(0.f, 1.f, 0.f, 20.f));
             }
-        });
+        }
     }
 }
